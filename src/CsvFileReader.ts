83fcd2bf6ffa650f7711324088ec5faec2008b88
Richard <richard.yang.tong@gmail.com>
2021-04-19 19:07:33 -0400

    Further converting the data types within each array.

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -14,6 +14,15 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
+      })
+      .map((row: string[]): any => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4])
+        ];
       });
     }
 }
