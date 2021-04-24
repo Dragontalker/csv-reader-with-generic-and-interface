ff594cc43f8977253b1ad003cb3acd1db1e13366
Richard <richard.yang.tong@gmail.com>
2021-04-24 09:41:07 -0400

    Added a new data source.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader';
 
 // Create an object that satisfies the 'DataReader' interface
-
+const csvFileReader = new CsvFileReader('football.csv');
 
 // Create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' interface
