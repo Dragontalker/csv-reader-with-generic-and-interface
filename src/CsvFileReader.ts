04fcad5cb68818ca7b8d55da0792b1d3ff6dfd24
Richard <richard.yang.tong@gmail.com>
2021-04-24 09:24:46 -0400

    Moved import of data format convertor.

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils'; 
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,3 +1,5 @@
+import { dateStringToDate } from './utils'; 
+
 interface DataReader {
   read(): void;
   data: string[][];
