8e0ea5899b6c4826ea72ff44ca04d2ccc3147d93
Richard <richard.yang.tong@gmail.com>
2021-04-24 18:47:34 -0400

    Moved and modified the existing code into run method.

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,4 @@ const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 // matchReader.matches
 
-let manUnitedWinds = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWinds++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWinds++;
-  }
-}
-
 console.log(`Man United wond ${manUnitedWinds} games`);
