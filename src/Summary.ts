4f7bb3402e96fcabb04d51d013d0ba34950cbfe6
Richard <richard.yang.tong@gmail.com>
2021-04-24 18:35:05 -0400

    Added a constructor.

--- src/Summary.ts
+++ src/Summary.ts
@@ -8,4 +8,8 @@ export interface OutputTarget {
   print(report: string): void;
 }
 
-export class Summary {}
+export class Summary {
+  constructor(
+    public analyzer: Analyzer, 
+    public outputTarget: OutputTarget) {}
+}
