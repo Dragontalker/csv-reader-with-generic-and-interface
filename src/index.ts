6e7ffde27fc5573bd3caf1ea688d7d0d16f7b295
Richard <richard.yang.tong@gmail.com>
2021-04-24 09:43:30 -0400

    Fixed export issue.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const csvFileReader = new CsvFileReader('football.csv');
 
 // Create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' interface
-const matchReader = 
+const matchReader = new MatchReader(csvFileReader);
 
 const reader = new MatchReader('football.csv');
 reader.read();
