137c17372c0082fc15534625c1371d7d62c17e94
Richard <richard.yang.tong@gmail.com>
2021-04-24 09:45:17 -0400

    Cleaned up.

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ matchReader.load();
 
 let manUnitedWinds = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWinds++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
