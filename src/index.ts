04ee6b63c6d3c2ced0fec2e1cd3611a88a257a3d
Richard <richard.yang.tong@gmail.com>
2021-04-24 18:48:17 -0400

    Cleaned up.

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,3 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 // matchReader.matches
-
-console.log(`Man United wond ${manUnitedWinds} games`);
