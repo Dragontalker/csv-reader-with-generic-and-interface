0b08ee147c7e16d938e9e163ec45189250668b2a
Richard <richard.yang.tong@gmail.com>
2021-04-24 09:17:41 -0400

    Rolled over.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
+import { MatchReader } from './inheritance/MatchReader';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
 let manUnitedWinds = 0;
