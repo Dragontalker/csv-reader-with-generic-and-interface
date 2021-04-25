2fd06a9b98fe1d3b89612b81714b32e9ace756b3
Richard <richard.yang.tong@gmail.com>
2021-04-24 20:34:50 -0400

    Call the new static method from main app.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,7 @@ import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 import { HtmlReport } from './reportTargets/HtmlReports';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
